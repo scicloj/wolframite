1e0a07e5b5821937538d4f1828b93840d5aec45c
Norman <orb@nostacktrace.com>
2015-03-16 19:29:51 -0500

    test clojure 1.6

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/orb/Clojuratica"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  ;; see README for info
                  [com.wolfram.jlink/JLink "4.4"]]
 
