468e81fbe498bc952496f03d37390c0fb06f6701
Christopher Small <metasoarous@gmail.com>
2021-10-29 17:54:47 -0700

    add deps.edn and try adding jlink through pomegranate

---
+++ src/clojuratica/jlink.clj
@@ -0,0 +1,9 @@
+(ns clojuratica.jlink
+  (:require [cemerick.pomegranate :as pom]))
+
+
+;(System/getenv "J)
+
+(pom/add-classpath "/usr/local/Wolfram/Mathematica/13.0/SystemFiles/Links/JLink/JLink.jar")
+  
+
