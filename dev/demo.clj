3599ff684c24d2aef476830a76de2fce5067ea1d
Pawel Ceranka <pawel.ceranka@gmail.com>
2021-11-03 16:54:04 +0000

    Dev: cleanup demo ns

--- dev/demo.clj
+++ dev/demo.clj
@@ -1,10 +1,7 @@
 (ns demo
   (:require
    [clojuratica.core :as wl :refer [WL]]
-   [clojuratica.base.convert :as convert]
-   [clojuratica.base.kernel :as kernel]
-   [clojuratica.runtime.dynamic-vars :as dynamic-vars]
-   [clojuratica.runtime.default-options :as default-options]))
+   graphics))
 
 
 ;; * Clojure a 1min intro
@@ -75,73 +72,63 @@
     (GeoNearest Here)
     WL)
 
-;; Wolfram Alpha
+;; - Wolfram Alpha
 
 (WL (WolframAlpha "number of moons of Saturn" "Result"))
 
-;; more examples
+;; - more examples
 
-;; dealing with WL's syntactic sugar
+;; - WL syntactic sugar
 (WL @(a b c d))
 
-;; more interesting examples...
+;; - more interesting examples...
 
 (WL (TextStructure "The cat sat on the mat."))
 
 (WL (TextStructure "You can do so much with the Wolfram Language." "ConstituentGraphs"))
 
-(wl/math-intern wl/math-evaluate :scopes)
-
+;; - Bidirectional translation
 
 (wl/wl->clj "GridGraph[{5, 5}]" wl/math-evaluate)
-
-(require 'graphics)
-
-(defn clojure->mathematica [clj-form & {:keys [output-fn]}]
-  (binding [dynamic-vars/*options* default-options/*default-options*
-            dynamic-vars/*kernel*  (kernel/kernel wl/kernel-link)]
-    (cond-> (convert/convert clj-form)
-      (ifn? output-fn) output-fn)))
 
 (wl/clj->wl '(GridGraph [5 5]) {:kernel-link wl/kernel-link
                                 :output-fn str})
 
+;; ** Graphics
+
+;; *** Init math canvas & app
+
 (def canvas (graphics/make-math-canvas! wl/kernel-link))
 (def app (graphics/make-app! canvas))
+
+;; *** Draw Something!
 
 (graphics/show! canvas (wl/clj->wl '(GridGraph [5 5]) {:output-fn str}))
 (graphics/show! canvas (wl/clj->wl '(ChemicalData "Ethanol" "StructureDiagram") {:output-fn str}))
 
+;; *** Make it easier (Dev Helper: closing over the canvas)
+
 (defn quick-show [clj-form]
   (graphics/show! canvas (wl/clj->wl clj-form {:output-fn str})))
 
+;; *** Some Simple Graphiscs Examples
+
 (quick-show '(ChemicalData "Ethanol" "StructureDiagram"))
 (quick-show '(GridGraph [5 5]))
+(quick-show '(GeoImage (Entity "City" ["NewYork" "NewYork" "UnitedStates"])))
 
+(comment ;; WIP
 
-(comment
-  (wl/wl->clj "GeoImage[Entity[\"City\", {\"NewYork\", \"NewYork\", \"UnitedStates\"}]]" wl/math-evaluate))
-  
-
-;; differen type of return values
-
-(WL :full-form (ChemicalData "Ethanol" "StructureDiagram"))
-
-(WL :no-parse (ChemicalData "Ethanol" "StructureDiagram"))
-
-(WL :as-function (ChemicalData "Ethanol" "StructureDiagram"))
-
-
-(comment
-  ;; TODO Having trouble with these
-  (quick-show '(GeoImage (Entity "City" ["NewYork" "NewYork" "UnitedStates"])))
+  ;; TODO: understand scopes
+  (wl/math-intern wl/math-evaluate :scopes)
 
   ;; TODO Fix this one
+  ;; prnc: need to understand this scoping construct better, seemed to work :/
   #_{:clj-kondo/ignore true}
   (Let [t (-> ["Text" "AliceInWonderland"]
               ExampleData
               ContentObject
               Snippet)]
-    (WordFrequency ~t (TextWords ~t)))
+       (WordFrequency ~t (TextWords ~t)))
 
   :end-comment)
