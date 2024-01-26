d496369f8d2c7903f81b4cbeaf8c52e5020faec0
Jakub Holy <jakubholy@jakubholy.net>
2024-01-26 18:20:07 +0100

    add test

--- test/wolframite/core_test.clj
+++ test/wolframite/core_test.clj
@@ -16,3 +16,19 @@
 ;;   (testing "translating FROM clj" (is (= "GridGraph[{5, 5}]"
 ;;                                          (wl/clj->wl '(GridGraph [5 5]) {:kernel-link wl/kernel-link
 ;;                                                                          :output-fn str})))))
+
+(deftest load-all-symbols-test
+  (wl/load-all-symbols 'w)
+  (is (= 3
+         (wl/eval (w/Plus 1 2)))
+      "An interned symbol can be used at a function position")
+  (is (= 3
+         (wl/eval (list 'Floor w/Pi)))
+      "An interned symbol can be used at a value position")
+  (is (= -3
+         (wl/eval (w/Floor (w/Plus 1 (w/Minus w/Pi))))
+         (wl/eval '(Floor (Plus 1 (Minus Pi)))))
+      "Interned vars behave properly also when nested few levels deep")
+  (is (= "x+y+z represents a sum of terms."
+         (-> #'w/Plus meta :doc))
+      "Interned vars have docstrings"))
