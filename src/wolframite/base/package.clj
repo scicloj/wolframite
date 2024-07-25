a125f8c5a184930a00664394d018a438109bf17a
light-matters <79052162+light-matters@users.noreply.github.com>
2024-07-25 15:33:03 +0200

    Update src/wolframite/base/package.clj
    Co-authored-by: Jakub Holý <jakubholy@jakubholy.net>

--- src/wolframite/base/package.clj
+++ src/wolframite/base/package.clj
@@ -29,16 +29,16 @@
            docs))))
 
 (defn load-package!
-  "An extended version of Wolfram's 'Get'. Gets a Wolfram package and adds the constants/functions etc. under a clojure-accessible symbol: either the given one or the name of the context by default.
+  "An extended version of Wolfram's 'Get'. Gets a Wolfram package and makes the constants/functions etc. accessible via a Clojure namespace (the given `alias`, by default same as `context`).
 
-  e.g. (<<! \"resources/WolframPackageDemo.wl\" \"WolframPackageDemo\" 'wp)
+  Example:  `(<<! \"./resources/WolframPackageDemo.wl\" \"WolframPackageDemo\" 'wp)`
 
-  path - string pointing to the package file
-  context - string for Wolfram context (essentially Wolfram's version of a namespace)
-  alias - clojure symbol to be used for accessing the Wolfram context. This will effectively be used like a clojure namespace, but where the clojure namespace need not exist.
-
-
-  TODO: Should check that the symbol isn't already being used."
+ - `path` - string pointing to the package file
+ - `context` - string for Wolfram context (essentially Wolfram's version of a namespace)
+ - `alias` - Clojure symbol to be used for accessing the Wolfram context. This will effectively become a Clojure namespace
+ 
+See [[intern-context!]] for details of turning the Wolfram context into a Clojure namespace."
+;; TODO: Should check that the symbol isn't already being used.
   ([path]
    (let [context (-> path fs/file-name fs/strip-ext)]
      (load-package! path context (symbol context))))
