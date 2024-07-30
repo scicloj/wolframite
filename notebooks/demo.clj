256b11a85ba2d189507276d3b196801bd8f3f049
ctw <clark.thomaswilliam@zoho.eu>
2024-07-30 21:11:31 +0200

    tidied up demo namespace

--- notebooks/demo.clj
+++ notebooks/demo.clj
@@ -10,21 +10,126 @@
    [wolframite.impl.jlink-instance :as jlink-instance]
    [wolframite.tools.hiccup :refer [view]]
    [wolframite.base.parse :as parse]
-   [scicloj.kindly.v4.kind :as kind]
-   [scicloj.kindly.v4.api :as kindly])
+   [scicloj.kindly.v4.kind :as k])
   (:import (java.awt Color Frame)
            (java.awt.event WindowAdapter ActionEvent)))
 
+(k/md "# Quickstart
+
+A brief demonstration of what Wolframite can do. It's a quick read, but you can also jump to any section of interest.")
+
+(k/md "## Init
+
+First, we must initialize Wolframite and connect it to a Wolfram kernel,
+which will perform the computations.")
+
+(wl/start)
+
+(k/md "If you get an error then please refer to the home README for further instructions. Your Wolfram installation is probably just in an unusual place and so you will have to provide the correct path.")
+
+(k/md "## First things first
+
+To check that the kernel is working, try the following command:")
+(wl/eval '(Dot [1 2 3] [4 5 6]))
+
+(k/md "As you can see, we can treat Wolfram commands similarly to unevaluated Clojure source code, with some predictable syntax adaptations. This works fine as a starting point, but dealing with long chains of raw symbols doesn't always scale well. Instead, you can also write")
+(wl/eval (w/Dot [1 2 3] [4 5 6]))
+(k/md "or, using one of our fancy aliases,")
+(wl/eval (w/<*> [1 2 3] [4 5 6]))
+(k/md ", which may be more familiar to the Mathematically inclined.
+
+Here, the 'w' namespace is a preconfigured, but configurable, intermediary to Wolfram's built-in symbols. This allows you to manipulate Wolfram functions just like any other clojure symbol (and get reliable editor autocompletion).")
+
+(k/md "## Code strings
+
+The above examples are the preferred ways to interop. You can however, use Wolfram command strings directly, e.g.")
+
+(wl/eval "{1 , 2, 3} . {4, 5, 6}")
+
+(k/md "## Explicit interning
+
+Although it shouldn't normally be necessary, we can also intern Wolfram functions more directly i.e. to effectively define Wolfram functions as clojure functions.
+
+The standard way of doing this is something like")
+(def greetings
+  (wl/eval
+   (w/fn [x] (w/StringJoin "Hello, " x "! This is a Mathematica function's output."))))
+(greetings "Stephen")
+
+(k/md "But this can also be done at a lower level, e.g.")
+(def W:Plus
+  (parse/parse-fn 'Plus {:jlink-instance (wolframite.impl.jlink-instance/get)}))
+(W:Plus 1 2 3)
+
+(k/md "## Bidirectional translation (experimental)
+
+Code translation in both directions is more difficult and is still somewhat experimental (especially in the wl->clj direction), but the basics work as expected, e.g.")
+
+(wl/->clj "GridGraph[{5, 5}]")
+(wl/->wl (w/GridGraph [5 5]) {:output-fn str})
+
+(k/md "## Graphics
+
+The above code however, within Mathematica, actually produces graphics. Does Wolframite support this?
+
+Yes!
+")
+
+(view (w/GridGraph [5 5]))
+
+(view (w/ChemicalData "Ethanol" "StructureDiagram"))
+
+(view (w/TextStructure "The cat sat on the mat."))
+
+(k/md "The above graphics were created using the")
 ^:kindly/hide-code
-(def md
-  (comp kindly/hide-code kind/md))
+(k/code "view")
+(k/md "function, as required above, and assumes that graphics are to be displayed in a browser.")
 
-(md "# Demonstration
+(k/md "## Computational knowledge
 
-A brief demonstration of Wolframite can do. We suggest reading it, but you can also jump to either of
-the following chapters.")
+Wolfram is also known for its dynamic or 'computational knowledge' engine.
 
+This can be accessed by many functions directly, ")
+
+(view (w/GeoNearest (w/Entity "Ocean") w/Here))
+(k/md ", or by posting a request to its online platform, Wolfram Alpha.")
+
+(wl/eval (w/WolframAlpha "number of moons of Saturn" "Result"))
+
+(view (w/WolframAlpha "number of moons of Saturn" "Result"))
+(k/md "Here, we've shown a response with and without the view function, for reference.")
+
+(k/md "## Mathematics and beyond
+
+In a nutshell, this is how Wolframite is normally used, but, of course, this barely scratches the surface.
+
+In particular, the flagship product of Wolfram, the one you've probably heard of, is Mathematica. And, as the name suggests, this entire system was built around the performance of abstract calculation and the manipulation of equations, e.g.
+")
+(-> (w/== 'E (w/* 'm (w/Power 'c 2)))
+
+    w/TeXForm
+    w/ToString
+    wl/eval
+    k/tex)
+
+(k/md "originally answered the question 'what is mass?'")
+
+(-> (w/== 'E (w/* 'm (w/Power 'c 2)))
+    (w/Solve 'm)
+    w/First w/First
+
+    w/TeXForm
+    w/ToString
+    wl/eval
+    k/tex)
+
+(k/md "This is where Wolfram, and so Wolframite, really shines. And if you're interested in exploring this further, have a gander at one of our longer tutorials.")
+
+^:kindly/hide-code
 (comment
+  ;; TODO: Don't hide code that generates TeX?
+  ;; TODO: not sure that this comment is needed anymore?
   (require '[scicloj.clay.v2.api :as clay])
   ;; Make HTML:
   (clay/make! {:source-path "dev/kindly_demo.clj"
@@ -38,64 +143,3 @@ the following chapters.")
                :clean-up-target-dir true
                :subdirs-to-sync []
                :format [:quarto :html]}))
-
-(md "## Init
-
-First, we must initialize Wolframite and connect it to a Wolfram kernel,
-which will perform the computations:")
-
-(wl/start)
-
-(md "## Base example")
-(wl/eval '(Dot [1 2 3] [4 5 6]))
-
-(md "This works fine, but if you don't want to deal with raw symbols, then you can also write")
-(wl/eval (w/Dot [1 2 3] [4 5 6]))
-(md "or, using our fancy aliases,")
-(wl/eval (w/<*> [1 2 3] [4 5 6]))
-(md ".")
-
-(md "## Strings of WL code")
-
-(wl/eval "{1 , 2, 3} . {4, 5, 6}")
-
-(md "## Def / intern WL fns, i.e. effectively define WL fns as clojure fns:")
-
-(def W:Plus (parse/parse-fn 'Plus {:jlink-instance (wolframite.impl.jlink-instance/get)}))
-
-(W:Plus 1 2 3) ; ... and call it
-
-(def greetings
-  (wl/eval
-   (w/fn [x] (w/StringJoin "Hello, " x "! This is a Mathematica function's output."))))
-
-(greetings "Stephen")
-
-(md "## Bidirectional translation
-(Somewhat experimental, especially in the wl->clj direction)")
-
-(wl/->clj "GridGraph[{5, 5}]")
-(wl/->wl (w/GridGraph [5 5]) {:output-fn str})
-
-(md "## Graphics")
-
-(view (w/GridGraph [5 5]))
-
-(view (w/GridGraph [5 5])
-      {:folded? true})
-
-(view (w/ChemicalData "Ethanol" "StructureDiagram"))
-
-(md "## More Working Examples")
-
-(wl/eval (w/GeoNearest (w/Entity "Ocean") w/Here))
-
-(md "TODO: Make this work with `view` as well.")
-
-(view (w/TextStructure "The cat sat on the mat."))
-
-(md "## Wolfram Alpha")
-
-(wl/eval (w/WolframAlpha "number of moons of Saturn" "Result"))
-
-(view (w/WolframAlpha "number of moons of Saturn" "Result"))
