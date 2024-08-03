dbb1d6e1fd154c516059008d12a4302eddc29d4d
Jakub Holy <jakubholy@jakubholy.net>
2024-08-03 11:16:06 +0200

    Add slide one

---
+++ notebooks/clojure_primer/index.clj
@@ -0,0 +1,15 @@
+^:kindly/hide-code
+(ns clojure-primer.index
+  "A very brief introduction to Clojure for a would-be users of Wolframite not familiar with the language"
+  (:require [scicloj.kindly.v4.kind :as k]))
+
+
+(k/md "# Clojure Primer
+
+A very brief introduction to Clojure for a would-be users of Wolframite not familiar with the language
+
+![Clojure syntax 1](./notebooks/clojure_primer/clj-syntax-slide-1.svg)
+
+TBD...")
+
+
