5084f7bfeb6b7db6fbad16b72a02091de567e5bc
Jakub Holy <jakubholy@jakubholy.net>
2024-08-03 22:32:31 +0200

    Add slide 3, some text

--- notebooks/clojure_primer/index.clj
+++ notebooks/clojure_primer/index.clj
@@ -8,16 +8,36 @@
 
 A very brief introduction to Clojure for a would-be users of Wolframite not familiar with the language
 
-![Clojure syntax 1](./notebooks/clojure_primer/clj-syntax-slide-1.svg)
+## Elementary syntax
 
-TBD
+Fortunately, Clojure syntax is very simple and consistent:
+")
 
-![Clojure syntax 2](./notebooks/clojure_primer/clj-syntax-slide-2.svg)
+^:kindly/hide-code
+(k/hiccup [:img {:src "./notebooks/clojure_primer/clj-syntax-slide-1.svg" :alt "Clojure syntax 1" :width "422" :height "150"}])
 
-TBD
+(k/md "Clojure is a Lisp, i.e. the code consists of the same data you use as a programmer.
 
-![Clojure syntax 3](./notebooks/clojure_primer/clj-syntax-slide-3.svg)
+Vectors are similar to JavaScript arrays, keywords to strings (but they’re magical and can be also used as a function that takes a map and looks up its value).
 
-TBD...")
+This is the syntax, i.e. how characters become data. Now, on to _semantics_, i.e. what does the data mean:")
 
+^:kindly/hide-code
+(k/hiccup [:img {:src "./notebooks/clojure_primer/clj-syntax-slide-2.svg" :alt "Clojure syntax 1" :width "586" :height "193"}])
 
+(k/md "A list is _interpreted_ as an invocation (e.g. a function call).
+
+For our purposes here, all invocable things - a function, a macro, a special form - are ± the same thing.")
+
+^:kindly/hide-code
+(k/hiccup [:img {:src "./notebooks/clojure_primer/clj-syntax-slide-3.svg" :alt "Clojure syntax 1" :width "640" :height "286"}])
+
+(k/md "Defining a function looks just the same. Highlights:
+
+`>`, `-` are function calls - there are no operators in Clojure. `if` is a \"special form\", i.e. just like a function call for our purposes.
+
+No `else`, no reserved words in Clojure. Everything returns a value.")
+
+(k/md "## Evaluation
+
+Code-as-data, ', quote ....")
