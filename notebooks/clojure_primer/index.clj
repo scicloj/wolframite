c54c322e7718305831e38c39c92ea9b8752469d4
Jakub Holy <jakubholy@jakubholy.net>
2024-09-20 14:52:32 +0200

    Wrap up Clojure primer

--- notebooks/clojure_primer/index.clj
+++ notebooks/clojure_primer/index.clj
@@ -11,14 +11,14 @@
 
 (k/md "# Clojure Primer
 
-A very brief introduction to Clojure for a would-be users of Wolframite not familiar with the language
+A very brief introduction to Clojure for a would-be users of Wolframite not familiar with the language.
 
 ## The essence of Clojure
 
 It's impossible to summarize any language in just a few words. But we can say that simplicity and expressiveness
 are some of the core values of Clojure. It has been designed to allow you to express your thoughts concisely,
 to write programs that avoid some common sources of errors (especially those caused by shared mutable data),
-and produce code that is well suited to evolve over time.
+and produce code that is well suited to evolution over time.
 
 ## Elementary syntax
 
@@ -29,17 +29,17 @@ looks like in Wolfram, Python, and Clojure:
 ^:kindly/hide-code
 (k/table
   [["Wolfram" (k/md "```wolfram
-  TODO
+  Join[{}, {\"first\", 2, <|\"name\" -> \"Ava\"|>}]
   ```")]
    ["Python" (k/md "```python
-  TODO
+  []+[\"first\",2,{\"name\": \"Ava\"}]
   ```")]
    ["Clojure" '(conj [] "first" 2 {"name" "Ava"})]])
 
 ;; We see here a few basic data structures: a `[vector]`, similar to Wolfram/Python lists, and a map `{"key": "value"}`,
 ;; similar to Python dictionaries / Wolfram associations. One interesting difference is that `,` commas in Clojure are optional.
 ;;
-;; `"Strings"` and numbers `1, 2, ...` are the same.
+;; `"Strings"` and numbers `1, 2, ...` are the same. Clojure's `nil` is just like Python's `None`
 ;;
 ;; Now, how would we **define a function** that sums two numbers?
 
@@ -61,35 +61,86 @@ looks like in Wolfram, Python, and Clojure:
 
 (k/md "Clojure is a Lisp, i.e. the code consists of the same data you use as a programmer: lists, vectors, maps, symbols, etc.
 
-Lists and vectors are both sequential data structures, but vectors support efficient access of any element, while lists are always read from start.
+Lists and vectors are both sequential data structures. Vectors support efficient access to any element, while lists are always read from start.
 
-Keywords are similar to strings, but tuned for use as keys in maps. (And a keyword may be used as a function that takes a map and looks up the corresponding value).
+Keywords are similar to strings, but tuned for use as keys in maps. Additionally, a keyword may be used as a function that takes a map and looks up the corresponding value:")
 
-This is the syntax, i.e. how characters become data. Now, on to _semantics_, i.e. what does the data mean:")
+(:name {:name "Ava"})
+
+(map :name [{:name "Ava"} {:name "Boudicca"}])
+
+;; This is the syntax, i.e. how characters become data. Now, on to _semantics_, i.e. what does the data mean:
 
 ^:kindly/hide-code
 (k/hiccup [:img {:src "./notebooks/clojure_primer/clj-syntax-slide-2.svg" :alt "Clojure syntax 1" :width "586" :height "193"}])
 
-(k/md "A list is _interpreted_ as an invocation (e.g. a function call).
-
-For our purposes here, all invocable things - functions, macros, and special forms - are ± the same thing.
+(k/md "
+A list is _interpreted_ as an invocation (e.g. a function call), where the first element is a symbol that resolves to something invocable
+(a function, a special form, or a macro; we don't need to care about their differences here). All the other elements are arguments to the function.
 
 The same code structure is used for everything, including defining functions:")
 
 ^:kindly/hide-code
 (k/hiccup [:img {:src "./notebooks/clojure_primer/clj-syntax-slide-3.svg" :alt "Clojure syntax 1" :width "640" :height "286"}])
 
-(k/md "Highlights:
+(k/md "
+The `defn`here defines a new function called `subtract-smaller`, taking two arguments `x` and `y`.
 
-`>`, `-` are just function calls. `if` is a \"special form\", i.e. just like a function call for our purposes.
+Noteworthy:
 
-No `else`, no reserved words in Clojure. Everything returns a value.")
+`>`, `-` are just function calls. `if` is a \"special form\" and `defn` is a macro, but they behave very similarly to functions.
+
+Notice there is no `else`. There are no reserved/special words in Clojure.
+")
+
+(k/md "## Basics
+
+* Everything returns a value (which may be `nil`). F.ex. `if` returns whatever the evaluated branch returned.
+* Clojure is [very tolerant of `nil`](https://ericnormand.me/article/nil-punning), _most_ functions treat it as an empty value - you can
+  `map` over it (getting `nil` back), append to it (getting back a list with the appended element) etc.
+* Whitespace is not significant. Commas are treated as a whitespace, and used occasionally for readability
+* Booleans: `true` and `false`. All predicates and conditional expressions treat both `false` and `nil` as `false` and
+ everything else as `true` (also called \"truthy\").
+* Clojure runs on the Java Virtual Machine (JVM) and you can directly call Java methods. (Learn more about [Clojure ↔ Java interoperability](https://clojure.org/reference/java_interop).)
+* Clojure is intended for [_interactive development_](https://clojure.org/guides/repl/introduction),
+  also known as REPL-driven development, where you build and evolve your application while it is running.
+  You may be used to a very similar this style of development from Python or Wolfram notebooks.
+")
 
 (k/md "## Evaluation
 
-Code-as-data, ', quote ....")
+Now is a good time to revisit [Understanding Wolframite](@sec-understanding-wolframite) and read about the three forms of Wolframite expressions
+(raw, evaluated, and Wolfram string).
 
-;; **TO BE CONTINUED**
+A point worth re-iterating is that Wolframite expressions are not evaluated by Clojure but are sent ± as-is to Wolfram for evaluation.
+Thus, you may not mix Clojure and Wolframite expressions freely. But there is one exception to that - a Wolframite expression may contain Clojure expressions
+that can be evaluated fully on the Clojure side and replaced with the result, before being sent to Wolfram. Let's have a look at a few examples:")
 
+;; Clojure-side only:
 (+ (* 3 (math/pow 2 2))) ; 12.0
+
+;; Wolfram-only:
 (wl/eval (w/+ (w/* 3 (w/Power 2 2))))
+
+;; Mixed, with Clojure evaluated before sending the form to Wolfram:
+(wl/eval (w/+ (w/* 3 (w/Power (+ 1 1) 2))))
+
+;; This is how the expression is evaluated before we resolve aliases and turn it into Wolfram and send it over to the kernel:
+
+(w/+ (w/* 3 (w/Power (+ 1 1) 2)))
+
+;; Notice that you may nest Clojure-only expression, which does not depend on the surrounding Wolfram context, inside a Wolframite expression,
+;; but you cannot do the opposite, i.e. nest a Wolframite expression inside a Clojure expression:
+(try
+ (wl/eval (+ (w/* 3 (w/Power (+ 1 1) 2))))
+ (catch Exception e
+   (str e)))
+
+;; This fail because we are passing a Wolframite expression (a list) to the Clojure `+` function, but it only works with numbers. We'd need to evaluate the expression first:
+
+(+ (wl/eval (w/* 3 (w/Power (+ 1 1) 2))))
+
+(k/md "## Resources for further learning
+
+* The [Clojure cheatsheat](http://jafingerhut.github.io/cheatsheet/clojuredocs/cheatsheet-tiptip-cdocs-summary.html)\nis a good place to look for functions.
+* [The 100 Most Used Clojure Expressions](https://ericnormand.me/article/100-most-used-clojure-expressions)")
