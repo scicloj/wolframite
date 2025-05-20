9e8d787348b3f469a1b4cacdbbd51d52df1ca936
Jakub Holý <jakubholy@jakubholy.net>
2025-05-20 22:31:40 +0200

    Ensure new graphics content is displayed; only scale output supporting it (#153)
    * Fix graphics to only scale scaleable expressions
    
    Do not wrap with `Show[...]` displayable, non-graphics elements that do not support this, such as `TextStructure`.
    
    TBD: Is there anything more than Graphics[3D] that supports Show? What about Image[3D]?
    
    * Fix 152: Ensure new graphics content is displayed
    
    Problem: Swing is not thread-safe, and according to internets, all changes to its state - i.e. components - need to be done on the Event Dispatch Thread. That is likely the reason why sometimes, calling `show!` did not update the already open window, or why it "flickered," showing briefly the new content than reverting to the old one.
    
    Fix: Run all the component-changing code inside E.D.T., via `SwingUtilities/invokeAndWait`.
    
    * Bonus: Don't use more space than we have when resizing.
    
    Though the scrollbars still do appear, so still Math is somehow bigger than the scoll pane 🤷
    
    * Fix resizing: setSize first, then setMathCmd
    
    I have finally figured out the correct incantation to make resizing work correctly. `Show[]` has nothing to do with it. What we must do is `math.setSize` _first_ and `.setMathCommand` _second_.
    
    Also: Factor out `ensure-math-sized-to-frame!`.

--- src/wolframite/tools/graphics.clj
+++ src/wolframite/tools/graphics.clj
@@ -11,7 +11,7 @@
     (java.awt.event ActionListener ComponentAdapter MouseAdapter)
     (java.awt.image BufferedImage)
     (javax.imageio ImageIO)
-    [javax.swing JFileChooser JFrame JMenuItem JOptionPane JPanel JPopupMenu JScrollPane Timer]))
+    [javax.swing JFileChooser JFrame JMenuItem JOptionPane JPanel JPopupMenu JScrollPane JViewport SwingUtilities Timer]))
 
 (defonce ^:private default-app (promise))
 
@@ -33,9 +33,37 @@
     (deliver default-app (make-app!)))
   @default-app)
 
-(defn- fit-graphic-expr-to-frame ^String [^String wl-expr-str ^Component container]
-  (let [size (.getSize container)]
-    (str "Show[" wl-expr-str ", ImageSize -> {" (.width size) "," (.height size) "}]")))
+; It seems that `Show[..]` has no effect on the resulting size, contrary to math.setSize
+;(defn- fit-graphic-expr-to-frame ^String [^String wl-expr-str ^Dimension size]
+;  (let [scaled-expr
+;        ;; With ImageSize->{w,h}, an object will always be drawn in a w×h region and will be sized to fit in the region.
+;        ;; If the object has a definite aspect ratio that is not h/w, then space will be left around it.
+;        (format "With[{g=%s}, If[MatchQ[g, _Graphics | _Graphics3D],Show[g,ImageSize -> {%d,%d}],g]]"
+;                wl-expr-str
+;                (- (.width size) 10)
+;                (- (.height size) 10))]
+;    scaled-expr))
+
+(defn- available-math-size [^JFrame _frame, ^Component math]
+  ;; BEWARE: In `componentResized`, it is likely that only the _frame_ & ScrollPane have been resized, not the math
+  (let [scroll-pane-viewport ^JViewport (.getParent math)]
+    (.getSize scroll-pane-viewport)))
+
+(defn- ensure-math-sized-to-frame!
+  ([math frame]
+   (let [new-math-size (available-math-size frame math)]
+     (doto math
+       ;; The next two are essential to resizing, in this particular order:
+       (.setSize new-math-size) ; this makes the content fit the new size
+       (.setMathCommand (.getMathCommand math)) ; seems pointless but it is necessary :'(
+       ;; And some supporting code
+       ;; We need to adjust the preferred size so that next time we call show! and thus
+       ;; frame.pack, we won't reset to the default preferred size but keep the size
+       ;; we've
+       (.setPreferredSize new-math-size)
+       ;; Not 100% sure we need all the following ones:
+       (.revalidate)
+       (.repaint)))))
 
 (defn set-resize-timer [{:keys [^JFrame frame, ^MathGraphicsJPanel math] :as _app} wl-expr-str]
   (doseq [l (.getComponentListeners frame)]
@@ -52,17 +80,7 @@
                   (some-> prev-timer .stop)
                   (doto (Timer. 200 ; wait 200ms before resizing, to make sure the user is done dragging
                                 (proxy [ActionListener] []
-                                  (actionPerformed [_]
-                                    (doto math
-                                      ;(.setLink kernel-link) ; unnecessary, we've set it already at start
-                                      (.setMathCommand (fit-graphic-expr-to-frame wl-expr-str math))
-                                      (.revalidate)
-                                      (.repaint))
-                                    ;; We need to adjust the preferred size so that next time we call show! and thus
-                                    ;; frame.pack, we won't reset to the default preferred size but keep the size
-                                    ;; we've
-                                    (.setPreferredSize math (.getSize frame))
-                                    )))
+                                  (actionPerformed [_] (ensure-math-sized-to-frame! math frame))))
                     (.setRepeats false)
                     (.start)))))))))
 
@@ -120,6 +138,29 @@
           (when (.isPopupTrigger e)
             (.show popup math (.getX e) (.getY e))))))))
 
+(defn- show-on-swing-thread [wl-expr-str {:keys [math frame] :as app} {:keys [jlink-instance scale-with-window?] :as _opts}]
+  (do
+    ;; Essential: this ensures sizes are >= min. size and thus non-zero, which we
+    ;; need for the size-related call(s) below
+    (.pack frame)
+    (doto math
+      (.setLink (proto/kernel-link jlink-instance))
+      (.setMathCommand wl-expr-str #_(fit-graphic-expr-to-frame wl-expr-str (available-math-size frame math)))
+      (ensure-math-sized-to-frame! frame)
+      (.setFocusable true)
+      (.requestFocusInWindow)))
+
+  (doto frame
+    (.setVisible true)
+    ;; TODO The window does not jump to the front, despite .toFront
+    ;; (depends on win. managers; may only work for windows in the same app...)
+    (.toFront))
+
+  (when scale-with-window?
+    (set-resize-timer app wl-expr-str))
+
+  (add-save-menu app))
+
 (defn show!
   "Display a graphical Wolfram expression result in a window - such as that of  `Plot[...]`.
   - `wl-expr` - Wolfram in a string or a Wolframite expression
@@ -137,7 +178,6 @@
 
   Returns a 'window' thing representing the window displaying the expression."
   ;; NOTE: Contrary to the legacy graphics, this uses the newer Swing and JLink's own math canvas component
-  ;; TODO Sometimes, the graphics is scaled to the frame, sometimes not; why/when?!
   ;; TODO: When we've multiple windows, should we try to position them not all at the same place?!
   ([wl-expr] (show! wl-expr (ensure-default-app!) nil))
   ([wl-expr window] (show! wl-expr window nil))
@@ -147,33 +187,14 @@
      :or {scale-with-window? true}
      :as _opts}]
    (let [jlink-instance' (or jlink-instance (jlink-instance/get))
-         {:keys [math frame] :as app} (or window (make-app!))
+         app (or window (make-app!))
          wl-expr-str (if (string? wl-expr)
                        wl-expr
                        (str (wl/->wl wl-expr {:jlink-instance jlink-instance'})))]
-
-     (do
-       ;; Essential: this ensures sizes are >= min. size and thus non-zero, which we
-       ;; need for the resizing call below
-       (.pack frame)
-       (doto math
-         (.setLink (proto/kernel-link jlink-instance'))
-         (.setMathCommand (fit-graphic-expr-to-frame wl-expr-str math))
-         (.revalidate)
-         (.repaint)
-         (.setFocusable true)
-         (.requestFocusInWindow)))
-
-     (doto frame
-       (.setVisible true)
-       ;; TODO The window does not jump to the front, despite .toFront
-       ;; (depends on win. managers; may only work for windows in the same app...)
-       (.toFront))
-
-     (when scale-with-window?
-       (set-resize-timer app wl-expr-str))
-
-     (add-save-menu app)
+     (SwingUtilities/invokeAndWait
+       ;; Swing is not thread-safe and changes to its components must be done on the Event Dispatch Thread (EDT)
+       (fn []
+         (show-on-swing-thread wl-expr-str app {:jlink-instance jlink-instance' :scale-with-window? scale-with-window?})))
 
      app)))
 
@@ -190,4 +211,17 @@
   (show! "Plot[Cos[x], {x, 0, 2 Pi}]" win)
   (show! "Plot[Sin[x], {x, 0, 2 Pi}]" win)
   (show! "Plot[Sin[x], {x, 0, 4 Pi}]" nil)
+
+  (apply available-math-size ((juxt :frame :math) @default-app))
+
+  (-> @default-app :math .repaint)
+  (-> @default-app :math .revalidate)
+  (-> @default-app :frame .getInsets)
+  (-> @default-app :math .getParent .getInsets)
+  ;; NOTE: Code changing components must be run on the Swing thread:
+  (SwingUtilities/invokeAndWait (fn [] (-> @default-app :frame .revalidate)))
+  (SwingUtilities/invokeAndWait (fn [] (-> @default-app :frame .repaint)))
+  (SwingUtilities/invokeAndWait (fn []
+                                  (let [{:keys [frame math]} @default-app]
+                                   (ensure-math-sized-to-frame! math frame))))
   )
